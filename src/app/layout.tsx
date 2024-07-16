67688673b9894810dc1c738f938620ea3f7cb901
Joseph Reyes <jreyes1919@gmail.com>
2024-07-16 18:36:21 -0500

    finished admin dashboard page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans", });
 
@@ -14,9 +15,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  function cn(arg0: string, variable: string): string | undefined {
-    throw new Error("Function not implemented.");
-  }
 
   return (
     <html lang="en">
